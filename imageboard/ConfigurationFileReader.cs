67e1f785f0b4344da1c907dbd2f56b6d7fc04850
nicoloison <nicole.loison@agilesoftwareengineering.com>
2014-10-21 16:33:10 -0600

    SoD.On(dictionar, dict) to be changed ! NOT WORKING

---
+++ imageboard/ConfigurationFileReader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using SOD_CS_Library;
+
+namespace ImageBoard
+{
+    class ConfigurationFileReader
+    {
+        /// <summary>
+        /// Gets the key (parameter) in the config file
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>String from config file</returns>
+        public string GetConfigSettings(string str)
+        {
+            return ConfigurationManager.AppSettings[str];
+        }
+
+        /// <summary>
+        /// Converts the string value associated with a key to a double
+        /// Can be easily casted as an int, float, etc.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>Double converted from config file</returns>
+        public double ConvertStringToDouble(string str)
+        {
+            return Convert.ToDouble(GetConfigSettings(str));
+        }
+
+        /// <summary>
+        /// Converts a string with multiple values associated with a key to Point3D
+        /// Delimeter is "," and converts string to a double
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="index"></param>
+        /// <returns>Point3D that is split and converted from config file</returns>
+        public Point3D ConvertStringToPoint(string str)
+        {
+
+            double[] points = new double[3];
+
+            string[] strArr = GetConfigSettings(str).Split(',');
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                points[i] = Convert.ToDouble(strArr[i]);
+            }
+
+            return new Point3D(points[0], points[1], points[2]);
+        }
+
+        public bool ConvertStringToBool(string str)
+        {
+            if (GetConfigSettings(str).Equals("true"))
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- imageboard/MainWindow.xaml.cs
+++ imageboard/MainWindow.xaml.cs
@@ -23,33 +23,37 @@ namespace ImageBoard
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region SOD.config set up
+        //ConfigFile reader instance
+        ConfigurationFileReader configreader;
 
-            /* PROCESS:
-            1. Add reference to the SoD dlls. Again, DLLS. Not the project.
-            2. Add static variables and change them according to the device
-            3. Put methods ConfigureSoD() and RegisterSoDEvents() into MainWindow()
-            4. Copy-paste the rest of the code
-            5. Implement ProcessDictionary(Dictionary) and/or ProcessString(String)
-            */
-            // Device parameters
-            static string _deviceID = "3"; // If it's not unique, it will be "randomly" assigned by locator.
-            static string _deviceName = "MAIN_WALLDISPLAY";
-            static string _deviceType = "WallDisplay";
-            static bool _deviceIsStationary = true; // If mobile device, assign false.
-            static double _deviceWidthInMM = 750 // Device width in millimetres
-                            , _deviceHeightInMM = 500 // Device height in millimetres
-                            , _deviceLocationX = 0 // Distance in metres from the sensor which was first connected to the server
-                            , _deviceLocationY = 0 // Distance in metres from the sensor which was first connected to the server
-                            , _deviceLocationZ = 0 // Distance in metres from the sensor which was first connected to the server
-                            , _deviceOrientation = 0 // Device orientation in Degrees, if mobile device, 0.
-                            , _deviceFOV = 180 // Device Field of View in degrees
-                            , _deviceObservableRange = 1.5;
-            // SOD parameters
-            static string _sodAddress = "beastwin.marinhomoreira.com"; // URL or IP
-            static int _sodPort = 5432;
-            // Grid URI
-            static string _sodGridUri = "http://" + _sodAddress + ":" + _sodPort + "/grid";
-            static TouchPoint touchStart;
+        // Device parameters
+        int _deviceID;
+        string _deviceName;
+        string _observerType;
+        bool _deviceIsStationary;
+        double _deviceWidthInMM
+                , _deviceHeightInMM
+                , _deviceLocationX
+                , _deviceLocationY
+                , _deviceLocationZ
+                , _deviceOrientation
+                , _deviceFOV
+                , _deviceObservableRange;
+        // SOD parameters
+        string _sodAddress;
+        int _sodPort;
+
+
+        // Device perimeter - used to tell if someone is close or far from the device.
+
+        private SOD SoD;
+        private string _deviceType;
+        #endregion
+
+        #region Image Specific
+
+        static TouchPoint touchStart;
             static bool isAlreadySwiped=false;
             static Dictionary<String,String> DictionaryOfImagesReceived= new Dictionary<String,String>();
             static int marginX=20;
@@ -58,16 +62,6 @@ namespace ImageBoard
             //static List<PersonPairing> _IdentifiedPeople = new List<PersonPairing>();
             // Enter and Leave Values
 
-            private SOD SoD;
-
-            public MainWindow()
-            {
-                InitializeComponent();
-                // SoD configuration with ip and port and Device configuration (type, dimensions, location, orientation...)
-                ConfigureSoD();
-                // Routes definition
-                RegisterSoDEvents();
-            }
 
             private string ImageToBase64(System.Drawing.Image img, System.Drawing.Imaging.ImageFormat format)
             {
@@ -83,9 +77,24 @@ namespace ImageBoard
                 }
                 throw new NotImplementedException();
             }
+        #endregion
+
+
+            public MainWindow()
+            {
+                InitializeComponent();
+                // SoD configuration with ip and port and Device configuration (type, dimensions, location, orientation...)
+                DefineSettings();
+                ConfigureSoD();
+                ConfigureDevice();
+                // Routes definition
+                RegisterSoDEvents();
+            }
             /// <summary>
             /// Region SoD contains SoD Configuration, Device Configuration, Definition of Routes and their handlers (e.g. sending notes)
             /// </summary>
+
+
             # region SoD
             private void ConfigureSoD()
             {
@@ -101,58 +110,72 @@ namespace ImageBoard
                 SoD.ownDevice.SetDeviceInformation(_deviceWidthInMM, _deviceHeightInMM, _deviceLocationX, _deviceLocationY, _deviceLocationZ, _deviceType, _deviceIsStationary);
                 SoD.ownDevice.orientation = _deviceOrientation;
                 SoD.ownDevice.FOV = _deviceFOV;
+                SoD.ownDevice.observer.observeRange = _deviceObservableRange;
+
+                SoD.ownDevice.ID = _deviceID;
+                SoD.ownDevice.name = _deviceName;
+            }
+
+            private void DefineSettings()
+            {
+                configreader = new ConfigurationFileReader();
+
+                // Device parameters
+                this._deviceWidthInMM = configreader.ConvertStringToDouble("widthInMM");
+                this._deviceHeightInMM = configreader.ConvertStringToDouble("heightInMM");
+                this._deviceLocationX = configreader.ConvertStringToDouble("locationX");
+                this._deviceLocationY = configreader.ConvertStringToDouble("locationY");
+                this._deviceLocationZ = configreader.ConvertStringToDouble("locationZ");
+                this._deviceOrientation = configreader.ConvertStringToDouble("orientation");
+                this._deviceFOV = configreader.ConvertStringToDouble("fieldOfView");
+                this._deviceID = (int)configreader.ConvertStringToDouble("_deviceID");
+                this._deviceName = configreader.GetConfigSettings("_deviceName");
+                this._deviceIsStationary = configreader.ConvertStringToBool("_deviceIsStationary");
+                this._observerType = configreader.GetConfigSettings("_observerType");
+                this._deviceObservableRange = configreader.ConvertStringToDouble("observableRange");
+                this._deviceType = configreader.GetConfigSettings("_deviceType");
+
+                this._sodAddress = configreader.GetConfigSettings("address");
+                this._sodPort = (int)configreader.ConvertStringToDouble("port");
+            }
+
+            private void ConfigureDevice()
+            {
+                // Configure device with its dimensions (mm), location in physical space (X, Y, Z in meters, from sensor), orientation (degrees), Field Of View (FOV. degrees) and name
+                SoD.ownDevice.SetDeviceInformation(_deviceWidthInMM, _deviceHeightInMM, _deviceLocationX, _deviceLocationY, _deviceLocationZ, _deviceType, _deviceIsStationary);
+                SoD.ownDevice.orientation = _deviceOrientation;
+                SoD.ownDevice.FOV = _deviceFOV;
 
                 // Name and ID of device - displayed in Locator
                 SoD.ownDevice.ID = _deviceID;
                 SoD.ownDevice.name = _deviceName;
-                SoD.ownDevice.observeRange = _deviceObservableRange;
             }
-        
-
 
             private void RegisterSoDEvents()
             {
                 // register for 'connect' event with io server
-                SoD.socket.On("connect", (data) =>
+                SoD.On("connect", (data) =>
                 {
                     Console.WriteLine("\r\nConnected...");
                     Console.WriteLine("Registering with server...\r\n");
                     SoD.RegisterDevice();  //register the device with server every time it connects or re-connects
                 });
 
-                SoD.socket.On("requestedData", (msgReceived) =>
+                SoD.On("request", (data) =>
                 {
-
+                    Console.WriteLine("Received request: " + data);
                 });
 
-                SoD.socket.On("dictionary", (dict) =>
+                SoD.On("dictionary", (dict) =>
                 {
+                    
                     Dictionary<string, dynamic> parsedMessage = SoD.ParseMessageIntoDictionary(dict);
                     Newtonsoft.Json.Linq.JObject o = parsedMessage["data"]["data"];
                     for(int i=1;i<=o.Count;i++){
                         DictionaryOfImagesReceived.Add(i.ToString(),(string) parsedMessage["data"]["data"][i.ToString()]);
                     }
-                    this.Dispatcher.Invoke((Action)(() =>
-                    {
-                        foreach (var element in DictionaryOfImagesReceived)
-                        {
-                            Image imgTransmission = new Image();
-                            imgTransmission.Source = ConvertDrawingImageToWPFImage(Base64ToImage(element.Value)).Source;
-                            imgTransmission.Height = 100;
-                            imgTransmission.Width = 100;
-                            imgCanvas.Children.Add(imgTransmission);
-                            imgTransmission.Margin = new Thickness(marginX, marginY, 0, 0);
-                            if ((marginX + 2 * imgTransmission.Width) > imgCanvas.ActualWidth)
-                            {
-                                marginY += (int)(1.5 * imgTransmission.Width);
-                                marginX = 20;
-                            }
-                            else
-                            {
-                                marginX += 150;
-                            }
-                        }
-                    }));
+                    ProcessDictionary();
+
                 });
 
                 // make the socket.io connection
@@ -161,15 +184,41 @@ namespace ImageBoard
 
             private void SendPicture()
             {
-                String[] selection = { "all" };
+                String[] selection = { "inView" };
                 String fileLocation = txtBrowse.Text; 
                 System.Drawing.Image img = System.Drawing.Image.FromFile(fileLocation);
                 String strToSend = ImageToBase64(img, img.RawFormat);
-                SoD.SendStringToDevices(strToSend, selection);
+                //SoD.SendStringToDevices(strToSend, selection);
+                SoD.SendToDevices.InView(strToSend, selection);
             }
 
             # endregion
 
+
+            private void ProcessDictionary() 
+            {
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    foreach (var element in DictionaryOfImagesReceived)
+                    {
+                        Image imgTransmission = new Image();
+                        imgTransmission.Source = ConvertDrawingImageToWPFImage(Base64ToImage(element.Value)).Source;
+                        imgTransmission.Height = 100;
+                        imgTransmission.Width = 100;
+                        imgCanvas.Children.Add(imgTransmission);
+                        imgTransmission.Margin = new Thickness(marginX, marginY, 0, 0);
+                        if ((marginX + 2 * imgTransmission.Width) > imgCanvas.ActualWidth)
+                        {
+                            marginY += (int)(1.5 * imgTransmission.Width);
+                            marginX = 20;
+                        }
+                        else
+                        {
+                            marginX += 150;
+                        }
+                    }
+                }));
+            }
             private void Grid_TouchDown(object sender, TouchEventArgs e)
             {
                 touchStart = e.GetTouchPoint(this);
@@ -216,9 +265,10 @@ namespace ImageBoard
             private void GetImages_Click(object sender, RoutedEventArgs e)
             {
                 // CANT gET THE SAMPLE CLIENT CODE TO WORK
-                String[] selection = { "all" };
+                String[] selection = { "inView" };
                 String strToSend = "GetImages";
-                SoD.SendStringToDevices(strToSend, selection);
+              //  SoD.SendStringToDevices(strToSend, selection);
+                SoD.SendToDevices.InView(strToSend, selection);
             }
 
             public System.Drawing.Image Base64ToImage(string base64String)
